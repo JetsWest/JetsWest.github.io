6dea13a85a67a40ea111ae623c9e29a32fdfa96d
Jackson <jratkins@cs.washington.edu>
2023-01-02 19:02:07 -0500

    Made big changes.

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    var btnContainer = document.getElementById("parent");
+    var btns = btnContainer.getElementsByClassName("btn");
+
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        })
+    }
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+})
+
+
