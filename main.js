79d0910f0533b6dc1fbb196dfd8d7707d50fdb97
Jackson Atkins <jraatkins@gmail.com>
2023-01-03 17:27:26 -0800

    Changed site name, added finishing touches.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const homepage = document.getElementById("homepage");
+const about = document.getElementById("about");
+const project = document.getElementById("project");
+const work = document.getElementById("work");
+
+
 document.addEventListener("DOMContentLoaded", function(event){
     var btnContainer = document.getElementById("parent");
     var btns = btnContainer.getElementsByClassName("btn");
@@ -20,6 +26,46 @@ document.addEventListener("DOMContentLoaded", function(event){
             });
         });
     });
+
+    homepage.addEventListener('mouseenter', (e) => {
+        for (var i = 0; i < btns.length; i++) {
+            if (btns[i].getAttribute("href").replace("#", "") === "homepage") {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                btns[i].className += " active";             
+            }
+        }
+    })
+
+    about.addEventListener('mouseenter', (e) => {
+        for (var i = 0; i < btns.length; i++) {
+            if (btns[i].getAttribute("href").replace("#", "") === "about") {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                btns[i].className += " active";             
+            }
+        }
+    })
+
+    project.addEventListener('mouseenter', (e) => {
+        for (var i = 0; i < btns.length; i++) {
+            if (btns[i].getAttribute("href").replace("#", "") === "project") {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                btns[i].className += " active";             
+            }
+        }
+    })
+
+    work.addEventListener('mouseenter', (e) => {
+        for (var i = 0; i < btns.length; i++) {
+            if (btns[i].getAttribute("href").replace("#", "") === "work") {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                btns[i].className += " active";             
+            }
+        }
+    })
 })
 
 
